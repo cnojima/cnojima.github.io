f7441dacc5f9c64c44421a6fef9a76ebbb063ace
Chris Nojima <mnojima@visa.com>
2022-11-02 10:19:49 -0700

    chore: port change

--- index.js
+++ index.js
@@ -7,4 +7,14 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  // CORS proxy
+  .get("/proxy", (req, res) => {
+    // read query parameters
+    const forwardUrl = req.query["url"];
+  
+    // make request to IEX API and forward response
+    request(forwardUrl, {
+      headers: req.headers
+    }).pipe(res);
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
